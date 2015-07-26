9ecdbd3f1158c1c1d713a57e2d180a99905ab4e8
Kristina Georgieva <kristina.georgieva@entelect.co.za>
2015-07-26 21:48:16 +0200

    Alignment changed and size of profile picture set

--- Web/dist/app/fines/controllers/fines.js
+++ Web/dist/app/fines/controllers/fines.js
@@ -6,6 +6,29 @@
 
     function Fines() {
         var vm = this;
-
+        
+        function AwardFine() {
+            toaster.pop('success', "Award Fine", "This will be a modal to award fines");
+        }
+        
+        function PayFine() {
+            toaster.pop('success', "Pay Fine", "This will be a modal to pay fines");
+        }
+        
+        function ViewUserStatistics() {
+            toaster.pop('success', "User Statustics", "This will be a modal to view the selected user's statistics");
+        }
+        
+        function SecondFine() {
+            toaster.pop('success', "Seconded Fine", "This will second a fine");
+        }
+        
+        function LikeFinePayment() {
+            toaster.pop('success', "Like", "This will like the payment of a fine");
+        }
+        
+        function DislikeFinePayment() {
+            toaster.pop('success', "Dislike", "This will dislike the payment of a fine");
+        }
     }
 }());
