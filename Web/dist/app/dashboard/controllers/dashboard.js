108f3ebb8a2400198c0ec35ff374b9a0bda292f3
prinay.panday <prinay.panday@entelect.co.za>
2015-07-26 20:06:47 +0200

    More dummy charts

--- Web/dist/app/dashboard/controllers/dashboard.js
+++ Web/dist/app/dashboard/controllers/dashboard.js
@@ -8,7 +8,7 @@
         var vm = this;
 
         vm.chartTypes = [
- 
+
             {
                 "id": "bar",
                 "title": "Bar"
@@ -25,10 +25,19 @@
         vm.chartSeries = [
             {
                 "name": "Fine Count",
-                "data": [1, 2, 4, 7, 3]
+                "data": [1, 2, 4, 7, 3],
+                "color": "green"
+
             }
   ];
 
+        vm.chartSeriesDistribution = [
+            {
+                "name": "Fine Count",
+                "data": [1, 2, 4, 7, 3, 8, 12, 4, 6, 7, 22, 30],
+                "color": "green"
+            }
+  ];
         vm.chartStack = [
             {
                 "id": '',
@@ -44,28 +53,31 @@
             }
   ];
 
-     
+
         vm.chartConfig = {
             options: {
                 chart: {
+                    backgroundColor: "#f4f4f4",
+                    plotShadow: true,
+                    renderTo: "panel",
                     type: 'bar'
                 },
-           
+
             },
             series: vm.chartSeries,
-                    xAxis: {
-                            categories: ['Prinay', 'Amrit', 'Kurt', 'Kristina', 'Kuben'],
-                            title: {
-                                text: 'Employee'
-                            }
-                        },
-                        yAxis: {
-                            title: {
-                                text: 'Number of Fines'
-                            }
-                        },
+            xAxis: {
+                categories: ['Prinay', 'Amrit', 'Kurt', 'Kristina', 'Kuben'],
+                title: {
+                    text: 'Employee'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Number of Fines'
+                }
+            },
             title: {
-                text: 'Leaderboard for 2015'
+                text: 'Leaderboard for 2015 - Top 5'
             },
             credits: {
                 enabled: true
@@ -74,6 +86,69 @@
             size: {}
         }
 
-   
+        vm.chartConfigDay = {
+            options: {
+                chart: {
+                    backgroundColor: "#f4f4f4",
+                    plotShadow: true,
+                    type: 'bar'
+                },
+
+            },
+            series: vm.chartSeries,
+            xAxis: {
+                categories: ['Prinay', 'Amrit', 'Kurt', 'Kristina', 'Kuben'],
+                title: {
+                    text: 'Employee'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Number of Fines'
+                }
+            },
+            title: {
+                text: 'Leaderboard for Today - Top 5'
+            },
+            credits: {
+                enabled: true
+            },
+            loading: false,
+            size: {}
+        }
+
+        vm.chartConfigDistribution = {
+            options: {
+                chart: {
+                    backgroundColor: "#f4f4f4",
+                    plotShadow: true,
+                    renderTo: 'panel',
+                    type: 'line'
+                },
+
+            },
+            series: vm.chartSeriesDistribution,
+
+            xAxis: {
+                categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                title: {
+                    text: 'Employee'
+                }
+            },
+            yAxis: {
+                title: {
+                    text: 'Number of Fines'
+                }
+            },
+            title: {
+                text: 'Fine Distribution 2015'
+            },
+            credits: {
+                enabled: true
+            },
+            loading: false,
+            size: {}
+        }
+
     }
 }());
