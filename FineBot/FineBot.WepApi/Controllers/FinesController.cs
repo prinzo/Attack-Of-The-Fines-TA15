80d73dc11b6cf65415ff9cad3162bad52965bc75
Kristina Georgieva <kristina.georgieva@entelect.co.za>
2015-07-29 22:24:28 +0200

    Basic functionality: adding a new fine trhough API. In progress: JSON object passed not gotten in API

---
+++ FineBot/FineBot.WepApi/Controllers/FinesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using FineBot.API.FinesApi;
+using FineBot.API.UsersApi;
+using FineBot.WepApi.Models;
+
+namespace FineBot.WepApi.Controllers
+{
+    public class FinesController : ApiController
+    {
+        private readonly IUserApi userApi;
+        private readonly IFineApi fineApi;
+
+        public FinesController(
+            IUserApi userApi, 
+            IFineApi fineApi
+            )
+        {
+            this.userApi = userApi;
+            this.fineApi = fineApi;
+        }
+
+        [HttpPost]
+        public bool IssueFine(NewFineModel newFine)
+        {
+            //FineModel fineModel = fineApi.IssueFine(new Guid(newFine.IssuerId), new Guid(newFine.RecipientId), newFine.Reason);
+
+            //return fineModel != null;
+
+            //todo figure out why json isn't being passed
+            return true;
+        }
+
+        [HttpGet]
+        public bool Hello() {
+           
+            return true;
+        }
+        
+    }
+}
---
+++ FineBot/FineBot.WepApi/Models/NewFineModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FineBot.WepApi.Models {
+    public class NewFineModel {
+
+        public String IssuerId { get; set; }
+
+        public String RecipientId { get; set; }
+
+        public String Reason { get; set; }
+    }
+}
