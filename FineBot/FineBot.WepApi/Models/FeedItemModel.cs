b589bba2d7569b4b9936b23fe972deb7622875bf
unknown <kristina.s.georgieva@gmail.com>
2015-07-31 15:45:21 +0200

    SAve call to API completed
    Started adding models for feed

---
+++ FineBot/FineBot.WepApi/Models/FeedItemModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FineBot.WepApi.Models {
+    public class FeedItemModel {
+        
+        public string Username { get; set; }
+        
+        public string UserGuid { get; set; }
+
+        public string Platform { get; set; }
+
+        public string IssuerName { get; set; }
+
+        public string IssuerGuid { get; set; }
+
+    }
+}
