519d95d86c052b8af8a442a8389fc35a6e3fdf5d
prinay.panday <prinay.panday@entelect.co.za>
2015-08-02 00:02:59 +0200

    Added dummy uploader using angular file upload on settings page

--- Web/app/settings/controllers/settings.js
+++ Web/app/settings/controllers/settings.js
@@ -16,12 +16,31 @@
         scope.user = [];
         scope.Name = '';
         scope.Surname = '';
+        scope.uploadButtonVisible = false;
+
         scope.uploader = new FileUploader();
         scope.UpdateUser = UpdateUser;
+        scope.upload = upload;
         GetUser();
 
+
+        scope.uploader.onSuccessItem = function (fileItem, response) {
+            toaster.success('File uploaded successfully', 'Successful Upload');
+            scope.uploadButtonVisible = true;
+        };
+
+        scope.uploader.onProgressItem = function () {
+            scope.uploadButtonVisible = true;
+
+        };
+
+        function upload(uploadItem) {
+            scope.uploadButtonVisible = false;
+            uploadItem.upload();
+        }
+
         function GetUser() {
-            scope.user = localStorageService.get('user'); 
+            scope.user = localStorageService.get('user');
         }
 
         function UpdateUser() {
