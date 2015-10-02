e2ea8d0e47d12b03e7a7e42a6a6a6c875c1b52e2
Amrit Purshotam <amrit.purshotam@gmail.com>
2015-10-02 08:53:30 +0200

    Added ResponseContext Extension for FormattedBotUserId

---
+++ FineBot/FineBot.BotRunner/Extensions/ResponseContextExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MargieBot.Models;
+
+namespace FineBot.BotRunner.Extensions
+{
+    public static class ResponseContextExtensions
+    {
+        public static string FormattedBotUserId(this ResponseContext context)
+        {
+            var builder = new StringBuilder();
+            builder.Append("<@");
+            builder.Append(context.BotUserID);
+            builder.Append(">");
+            
+            return builder.ToString();
+        }
+    }
+}
