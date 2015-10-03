63f623c7bfb50173cd7f9c388c4d1cf974037448
amrit.purshotam <amrit.purshotam@entelect.co.za>
2015-10-03 09:42:59 +0200

    Add FineCountResponder (wip)

---
+++ FineBot/FineBot.BotRunner/Responders/FineCountResponder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using FineBot.BotRunner.Responders.Interfaces;
+using MargieBot.Models;
+
+namespace FineBot.BotRunner.Responders
+{
+    public class FineCountResponder : IFineBotResponder
+    {
+        public bool CanRespond(ResponseContext context)
+        {
+            return false;
+        }
+
+        public BotMessage GetResponse(ResponseContext context)
+        {
+            return new BotMessage{ Text = "in progress" };
+        }
+    }
+}
