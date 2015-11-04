1c0f2708a0f362d01b49e7591f61403e565c441a
prinay.panday <prinay.panday@entelect.co.za>
2015-11-04 22:20:58 +0200

    Updated placeholders

--- FineBot/FineBot.WepApi/Global.asax.cs
+++ FineBot/FineBot.WepApi/Global.asax.cs
@@ -35,7 +35,7 @@ namespace FineBot.WepApi
             HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
             if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
             {
-                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "POST, PUT, DELETE");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 
                 HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
                 HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
