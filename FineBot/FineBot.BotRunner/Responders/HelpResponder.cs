86e4187032094c108eadaebed0251104bb46c67c
amrit.purshotam <amrit.purshotam@entelect.co.za>
2015-11-08 15:47:07 +0200

    Update help responder with new commands

--- FineBot/FineBot.BotRunner/Responders/HelpResponder.cs
+++ FineBot/FineBot.BotRunner/Responders/HelpResponder.cs
@@ -30,7 +30,9 @@ namespace FineBot.BotRunner.Responders
             builder.AppendLine();
             builder.AppendLine("@finesbot leaderboard <today|week|month|year>: Leaderboard of the top five users with the most fines for the current day/week/month/year. If no time period is specified, the all time leaderboard is displayed");
             builder.AppendLine();
-            builder.AppendLine("@finesbot count all: Counts the number of fines successfully issued (i.e. seconded) by all users.");
+            builder.AppendLine("@finesbot fine count <all|users>: Counts the number of fines successfully issued (i.e. seconded) by the specified users. If \"all\" is specified the count for all fines will be displayed. If no users are specified, the fine count of the requester will be displayed.");
+            builder.AppendLine();
+            builder.AppendLine("@finesbot seconded count <users>: Counts the number of fines seconded by the specified users. If no user is specified, the number of fines seconded by the requester will be displayed.");
             builder.AppendLine();
             builder.AppendLine(String.Format("@FinesBot support <support type: {0}> <message>: Creates a support ticket with the status selected. If no status is specified it is created as a general issue.", String.Join("|", Enum.GetNames(typeof(SupportType)))));
 
